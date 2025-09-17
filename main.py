3a1a7542aa5af85629866adf11266352919f26ed
Tijana Tuka <tuka.tijana@gmail.com>
2025-09-17 20:26:06 +0200

    Faza 6: Dodata klasa Cart i funkcionalnost korpe u main.py

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 # Kreiramo ProductManager
 manager = ProductManager()
 
-# Dodajemo proizvode
+# Dodajemo proizvode u inventar
 manager.add_product(Product("Laptop", 80000, 5))
 manager.add_product(Product("Telefon", 50000, 10))
 manager.add_product(Product("Slusalice", 3000, 50))
 
-# Prikaz svih proizvoda
+# Prikaz proizvoda i ukupne vrednosti
 print("Proizvodi u inventaru:")
 manager.show_products()
-
-# Prikaz ukupne vrednosti
 print(f"Ukupna vrednost inventara: {manager.total_value()} RSD")
 
-# Uklanjanje proizvoda "Slusalice"
+# Kreiramo korpu
+shopping_cart = Cart()
+
+# Dodajemo proizvode u korpu pre uklanjanja
+shopping_cart.add_to_cart(manager.products[0], 2)  # Laptop
+shopping_cart.add_to_cart(manager.products[1], 1)  # Telefon
+shopping_cart.add_to_cart(manager.products[2], 5)  # Slusalice
+
+print("\nSadržaj korpe:")
+shopping_cart.show_cart()
+print(f"Ukupna vrednost za naplatu: {shopping_cart.total_amount()} RSD")
+
+# Uklanjanje proizvoda iz inventara
 manager.remove_product("Slusalice")
+
 print("\nNakon uklanjanja proizvoda 'Slusalice':")
 manager.show_products()
-
+print(f"Ukupna vrednost inventara: {manager.total_value()} RSD")
